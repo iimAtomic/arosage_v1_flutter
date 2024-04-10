6d6a5610b8be46140dd4325a272538e1156db9eb
iimAtomic <vegbahermerite@gmail.com>
2024-04-10 11:43:09 +0200

    fil d'actu

--- lib/views/PlantesFeedPage.dart
+++ lib/views/PlantesFeedPage.dart
@@ -1,51 +1,220 @@
-// home_page.dart
-import 'package:flutter/material.dart';
 import 'dart:convert';
-import '../services/fetchImage.dart';
-import '../models/photo.dart';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:arosagev1_flutter/models/photo.dart';
+import 'package:arosagev1_flutter/storage/storage.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+
+import 'custom_drawer.dart';
 
 class PlantesFeed extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _PlantesPageState createState() => _PlantesPageState();
 }
 
-class _HomePageState extends State<PlantesFeed> {
-  final ApiService _apiService = ApiService();
-  List<Photo> _photos = [];
+class _PlantesPageState extends State<PlantesFeed> {
+  final _formKey = GlobalKey<FormState>();
+  List<dynamic> _plantes = [];
+  String _nom = '';
+  String _desc = '';
+  XFile? _image;
 
   @override
   void initState() {
     super.initState();
-    _loadPhotos();
+    _fetchPlantes();
   }
 
-  void _loadPhotos() async {
-    try {
-      final photos = await _apiService.getPhotosOfPlant(1); // Utilisez l'ID de plante approprié
-      setState(() {
-        _photos = photos;
-      });
-    } catch (e) {
-      print("Failed to load photos: $e");
+  Future<void> _fetchPlantes() async {
+    var url = Uri.parse(
+        'http://ec2-13-39-86-184.eu-west-3.compute.amazonaws.com/api/user/v1/plante');
+    var pseudo = await SecureStorage().readSecureData("pseudo");
+    var response = await http.get(
+      url,
+      headers: {"pseudo": pseudo},
+    );
+    if (response.statusCode == 200) {
+      print("il existe des plantes ");
+      List<dynamic> plantesData = json.decode(response.body);
+
+      // Clear the _plantes list before populating it with new data
+      _plantes.clear();
+
+      for (var planteData in plantesData) {
+        var planteId = planteData["id"];
+        var plantePhotos = await _fetchPlantePhotos(planteId);
+        List<Map<String, dynamic>> photoDataList = [];
+        for (var photo in plantePhotos) {
+          photoDataList.add({
+            "name": photo.name,
+            "type": photo.type,
+            "size": photo.size,
+            "data": photo.data,
+          });
+        }
+        _plantes.add({
+          "id": planteId,
+          "nom": planteData["nom"],
+          "description": planteData["description"],
+          "photoData": photoDataList,
+        });
+      }
+      setState(() {});
+    } else {
+      print("Erreur lors de la récupération des plantes");
     }
   }
+
+  Future<List<PhotoAro>> _fetchPlantePhotos(int planteId) async {
+    var url = Uri.parse(
+        'http://ec2-13-39-86-184.eu-west-3.compute.amazonaws.com/api/plante/v2/images');
+    var response = await http.get(
+      url,
+      headers: {"planteId": planteId.toString()},
+    );
+    if (response.statusCode == 200) {
+      List<dynamic> jsonData = json.decode(response.body);
+      List<PhotoAro> photos = [];
+      for (var item in jsonData) {
+        photos.add(PhotoAro.fromJson(item));
+      }
+      return photos;
+    } else {
+      throw Exception("Error retrieving photos");
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Photos comme publications FB'),
+        title: Text('Fil d\'actualité'),
+        backgroundColor: Colors.blue, // Style Facebook
       ),
       body: ListView.builder(
-        itemCount: _photos.length,
+        itemCount: _plantes.length,
         itemBuilder: (context, index) {
-          final photo = _photos[index];
-          return ListTile(
-            title: Text(photo.name),
-            subtitle: Image.memory(base64Decode(photo.data)),
+          var plante = _plantes[index];
+          return PlantPostCard(
+            nom: plante['nom'],
+            description: plante['description'],
+            imageData: plante['photoData'].isNotEmpty ? plante['photoData'][0]['data'] : null,
           );
         },
       ),
     );
+  }
+}
+
+class PlantPostCard extends StatelessWidget {
+  final String nom;
+  final String description;
+  final Uint8List? imageData;
+
+  const PlantPostCard({
+    Key? key,
+    required this.nom,
+    required this.description,
+    this.imageData,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2.0,
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+      child: Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              child: Icon(Icons.person), // Remplacez par l'image de l'utilisateur si disponible
+            ),
+            title: Text(nom),
+            subtitle: Text(description),
+            trailing: Icon(Icons.more_vert),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(description),
+          ),
+          imageData != null
+              ? Image.memory(imageData!)
+              : Placeholder(fallbackHeight: 200), // Hauteur à ajuster
+          ButtonBar(
+            alignment: MainAxisAlignment.start,
+            children: [
+              TextButton.icon(
+                icon: Icon(Icons.thumb_up, color: Colors.blue),
+                label: Text('J\'aime'),
+                onPressed: () {
+                  // Action pour 'J'aime'
+                },
+              ),
+              TextButton.icon(
+                icon: Icon(Icons.comment, color: Colors.grey),
+                label: Text('Commenter'),
+                onPressed: () {
+                  // Action pour commenter
+                },
+              ),
+              TextButton.icon(
+                icon: Icon(Icons.share, color: Colors.grey),
+                label: Text('Partager'),
+                onPressed: () {
+                  // Action pour partager
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+class PhotoAro {
+  late String name;
+  late String type;
+  late int size;
+  late Uint8List data;
+
+  PhotoAro(
+      {required this.name,
+      required this.type,
+      required this.size,
+      required this.data});
+
+  factory PhotoAro.fromJson(Map<String, dynamic> json) {
+    if (json.containsKey('data') && json['data'] != null) {
+      try {
+        // Decode the base64 encoded data to Uint8List
+        Uint8List decodedData = base64.decode(json['data']);
+        return PhotoAro(
+          name: json['name'],
+          type: json['type'],
+          size: json['size'],
+          data: decodedData,
+        );
+      } catch (e) {
+        print('Error decoding base64 data: $e');
+        // Return a default PhotoAro object with empty data if decoding fails
+        return PhotoAro(
+          name: json['name'],
+          type: json['type'],
+          size: json['size'],
+          data: Uint8List(0), // Empty Uint8List
+        );
+      }
+    } else {
+      // Return a default PhotoAro object with empty data if 'data' field is missing or null
+      return PhotoAro(
+        name: json['name'],
+        type: json['type'],
+        size: json['size'],
+        data: Uint8List(0), // Empty Uint8List
+      );
+    }
   }
 }
--- lib/views/ProfilePage.dart
+++ lib/views/ProfilePage.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:arosagev1_flutter/models/photo.dart';
+import 'package:arosagev1_flutter/storage/storage.dart';
 import 'package:flutter/material.dart';
-import 'dart:ui'; // Importez dart:ui pour utiliser ImageFilter.blur
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+import 'dart:ui';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -8,11 +15,83 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   String? _selectedImage;
+  final _formKey = GlobalKey<FormState>();
+  List<dynamic> _plantes = [];
+  String _nom = '';
+  String _desc = '';
+  XFile? _image;
 
-  void _toggleImage(String? assetName) {
-    setState(() {
-      _selectedImage = assetName;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _fetchPlantes();
+  }
+
+  Future<void> _fetchPlantes() async {
+    var url = Uri.parse(
+        'http://ec2-13-39-86-184.eu-west-3.compute.amazonaws.com/api/user/v1/plante');
+    var pseudo = await SecureStorage().readSecureData("pseudo");
+    var response = await http.get(
+      url,
+      headers: {"pseudo": pseudo},
+    );
+    if (response.statusCode == 200) {
+      print("il existe des plantes ");
+      List<dynamic> plantesData = json.decode(response.body);
+
+      // Clear the _plantes list before populating it with new data
+      _plantes.clear();
+
+      for (var planteData in plantesData) {
+        var planteId = planteData["id"];
+        var plantePhotos = await _fetchPlantePhotos(planteId);
+        List<Map<String, dynamic>> photoDataList = [];
+        for (var photo in plantePhotos) {
+          photoDataList.add({
+            "name": photo.name,
+            "type": photo.type,
+            "size": photo.size,
+            "data": photo.data,
+          });
+        }
+        _plantes.add({
+          "id": planteId,
+          "nom": planteData["nom"],
+          "description": planteData["description"],
+          "photoData": photoDataList,
+        });
+      }
+      setState(() {});
+    } else {
+      print("Erreur lors de la récupération des plantes");
+    }
+  }
+
+  Future<List<PhotoAro>> _fetchPlantePhotos(int planteId) async {
+    var url = Uri.parse(
+        'http://ec2-13-39-86-184.eu-west-3.compute.amazonaws.com/api/plante/v2/images');
+    var response = await http.get(
+      url,
+      headers: {"planteId": planteId.toString()},
+    );
+    if (response.statusCode == 200) {
+      List<dynamic> jsonData = json.decode(response.body);
+      List<PhotoAro> photos = [];
+      for (var item in jsonData) {
+        photos.add(PhotoAro.fromJson(item));
+      }
+      return photos;
+    } else {
+      throw Exception("Error retrieving photos");
+    }
+  }
+
+  void _toggleImage(String? imageUrl) {
+    if (imageUrl != null) {
+      setState(() {
+        _selectedImage = imageUrl;
+      });
+    }
   }
 
   @override
@@ -78,15 +157,23 @@ class _ProfilePageState extends State<ProfilePage> {
                       crossAxisSpacing: 8,
                       mainAxisSpacing: 8,
                     ),
-                    itemCount: 9,
+                    itemCount:
+                        _plantes.length, // Use the length of your plants list
                     itemBuilder: (context, index) {
-                      String imageAsset = 'assets/plante${index + 1}.jpeg';
+                      var plant = _plantes[index];
+                      var photoDataList =
+                          plant['photoData'] as List<Map<String, dynamic>>;
+                      var imageUrl = photoDataList.isNotEmpty
+                          ? photoDataList[0]['data']
+                          : null; // Assuming 'data' holds the URL to the image
+
                       return GestureDetector(
-                        onTap: () => _toggleImage(imageAsset),
+                        onTap: () => _toggleImage(imageUrl),
                         child: Container(
                           decoration: BoxDecoration(
                             image: DecorationImage(
-                              image: AssetImage(imageAsset),
+                              image: NetworkImage(imageUrl ??
+                                  ''), // Use NetworkImage for images from the network
                               fit: BoxFit.cover,
                             ),
                             borderRadius: BorderRadius.circular(8),
@@ -105,19 +192,19 @@ class _ProfilePageState extends State<ProfilePage> {
               ],
             ),
             if (_selectedImage != null) ...[
-              Positioned.fill(
-                child: GestureDetector(
-                  onTap: () => _toggleImage(null),
-                  child: Container(
-                    color: Colors.black.withOpacity(0.5),
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Image.asset(
-                          _selectedImage!,
-                          fit: BoxFit.contain,
-                        ),
+            Positioned.fill(
+              child: GestureDetector(
+                onTap: () => _toggleImage(null),
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Image.network(
+                        _selectedImage!,
+                        fit: BoxFit.contain,
+                      ),
                       ),
                     ),
                   ),
@@ -145,5 +232,50 @@ class _ProfilePageState extends State<ProfilePage> {
         side: BorderSide(color: Colors.grey),
       ),
     );
+  }
+}
+
+class PhotoAro {
+  late String name;
+  late String type;
+  late int size;
+  late Uint8List data;
+
+  PhotoAro(
+      {required this.name,
+      required this.type,
+      required this.size,
+      required this.data});
+
+  factory PhotoAro.fromJson(Map<String, dynamic> json) {
+    if (json.containsKey('data') && json['data'] != null) {
+      try {
+        // Decode the base64 encoded data to Uint8List
+        Uint8List decodedData = base64.decode(json['data']);
+        return PhotoAro(
+          name: json['name'],
+          type: json['type'],
+          size: json['size'],
+          data: decodedData,
+        );
+      } catch (e) {
+        print('Error decoding base64 data: $e');
+        // Return a default PhotoAro object with empty data if decoding fails
+        return PhotoAro(
+          name: json['name'],
+          type: json['type'],
+          size: json['size'],
+          data: Uint8List(0), // Empty Uint8List
+        );
+      }
+    } else {
+      // Return a default PhotoAro object with empty data if 'data' field is missing or null
+      return PhotoAro(
+        name: json['name'],
+        type: json['type'],
+        size: json['size'],
+        data: Uint8List(0), // Empty Uint8List
+      );
+    }
   }
 }
